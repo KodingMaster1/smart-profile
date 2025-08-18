3ea78c340c9af01b81287493bdc885905a0c0792
Laurian Lawrence <laurianlawrence26@gmail.com>
2025-08-18 20:27:21 +0300

    Auto-save: 2025-08-18T17-27-21-098Z

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['localhost', 'your-project-id.supabase.co'],
-  },
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
   },
 }
 
